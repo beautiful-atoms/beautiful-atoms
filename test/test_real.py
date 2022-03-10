e9bca8c67ff3a2cdc3352f796d01ad5a474ee279
superstar54 <xingwang1991@gmail.com>
2022-03-10 17:34:53 +0100

    skip test_real

--- test/test_real.py
+++ test/test_real.py
@@ -6,7 +6,7 @@ from batoms import Batoms
 from batoms.utils.butils import removeAll
 from ase.build import graphene_nanoribbon
 
-
+@pytest.mark.skip(reason="Need to run manualy by mouse")
 def test_force_field():
     bpy.ops.batoms.delete()
     gnr = graphene_nanoribbon(2, 2, type="armchair", saturated=True, vacuum=3.5)
@@ -14,6 +14,7 @@ def test_force_field():
     gnr = Batoms("gnr", from_ase=gnr)
 
 
+@pytest.mark.skip(reason="Need to run manualy by mouse")
 def test_force_field_al():
     bpy.ops.batoms.delete()
     al = Batoms("al", from_ase=bulk("Al"))
