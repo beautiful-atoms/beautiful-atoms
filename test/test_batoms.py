097eeb8eacbb2591480595cd8b997d11ef1b42af
Xing Wang <xingwang1991@gmail.com>
2022-03-11 04:28:11 +0100

    Update test_batoms.py

--- test/test_batoms.py
+++ test/test_batoms.py
@@ -4,7 +4,16 @@ from batoms import Batoms
 import numpy as np
 import pytest
 
+try:
+    from pytest_blender.test import pytest_blender_unactive
+except ImportError:
+    pytest_blender_unactive = False
 
+
+@pytest.mark.skipif(
+    pytest_blender_unactive,
+    reason="Requires testing loading the pytest-blender plugin.",
+)
 def test_empty():
     """Create an empty Batoms object"""
     bpy.ops.batoms.delete()
