339593f107f96ff9faf7c82464e986b769bf42de
T.Tian <alchem0x2a@gmail.com>
2022-06-06 15:24:22 -0400

    initial test on logger

--- tests/test_preference.py
+++ tests/test_preference.py
@@ -1,19 +1,47 @@
 import bpy
 import pytest
+import logging
 
 
 package = "batoms"
+addon = bpy.context.preferences.addons[package]
+preferences = addon.preferences
+
+def create_molecule():
+    bpy.ops.batoms.delete()
+    from batoms import Batoms
+    from ase.build import molecule
+    batoms = Batoms(from_ase=molecule("CO"))
+    bpy.ops.batoms.delete()
+
 
 def test_logging_level():
-    """logging level
+    """logging level. 
+    Since now all the child loggers are "delegation to the parent" 
+    https://docs.python.org/3/library/logging.html#logging.Logger.setLevel, 
+    explicitly testing logger.level will not work. Instead, use getEffectiveLevel to check 
+    the effect of propagation
     """
-    bpy.context.preferences.addons[package].preferences.logging_level = "DEBUG"
     from batoms.batoms import logger
-    assert(logger.level == 10)
-    bpy.context.preferences.addons[package].preferences.logging_level = "INFO"
-    assert(logger.level == 20)
-    bpy.context.preferences.addons[package].preferences.logging_level = "WARNING"
-    assert(logger.level == 30)
+
+    for level in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
+        preferences.logging_level = level
+        assert(logger.getEffectiveLevel() == getattr(logging, level))
+    # bpy.context.preferences.addons[package].preferences.logging_level = "INFO"
+    # assert(logger.level == 20)
+    # bpy.context.preferences.addons[package].preferences.logging_level = "WARNING"
+    # assert(logger.level == 30)
+
+def test_logging_level_emit(caplog):
+    """Test if setting logging level hierachically works
+    """
+    # caplog.set_level(logging.DEBUG)
+    create_molecule()
+    root_logger = logging.getLogger("batoms")
+    # root_logger.
+
+    
+    
 
 
 if __name__ == "__main__":
