a9f3fae1829fef09e3ee4e653f4449c02237ec25
superstar54 <xingwang1991@gmail.com>
2022-06-07 15:23:35 +0200

    add helper function and get_active_cell

--- batoms/gui/gui_cell.py
+++ batoms/gui/gui_cell.py
@@ -1,12 +1,14 @@
 import bpy
 from bpy.types import Panel
 from bpy.props import (BoolProperty,
+                       FloatProperty,
                        FloatVectorProperty,
                        IntVectorProperty
                        )
 
 from batoms import Batoms
-
+from batoms.cell import Bcell
+from batoms.gui.gui_batoms import get_active_batoms
 
 class Cell_PT_prepare(Panel):
     bl_label = "Cell"
@@ -20,13 +22,23 @@ class Cell_PT_prepare(Panel):
         layout = self.layout
         clpanel = context.scene.clpanel
 
-        op = layout.operator("batoms.apply_cell")
-        op.a = clpanel.cell_a
-        op.b = clpanel.cell_b
-        op.c = clpanel.cell_c
-        layout.prop(clpanel, "cell_a")
-        layout.prop(clpanel, "cell_b")
-        layout.prop(clpanel, "cell_c")
+        # op = layout.operator("batoms.apply_cell")
+        # op.a = [clpanel.cell_a0, clpanel.cell_a1, clpanel.cell_a2]
+        # op.b = [clpanel.cell_b0, clpanel.cell_b1, clpanel.cell_b2]
+        # op.c = [clpanel.cell_c0, clpanel.cell_c1, clpanel.cell_c2]
+        box = layout.box()
+        row = box.row(align = True)
+        row.prop(clpanel, "cell_a0")
+        row.prop(clpanel, "cell_a1")
+        row.prop(clpanel, "cell_a2")
+        row = box.row(align = True)
+        row.prop(clpanel, "cell_b0")
+        row.prop(clpanel, "cell_b1")
+        row.prop(clpanel, "cell_b2")
+        row = box.row(align = True)
+        row.prop(clpanel, "cell_c0")
+        row.prop(clpanel, "cell_c1")
+        row.prop(clpanel, "cell_c2")
         layout.prop(clpanel, "pbc")
 
         op = layout.operator("batoms.apply_transform")
@@ -45,6 +57,27 @@ class Cell_PT_prepare(Panel):
         layout.prop(clpanel, "boundary_b")
         layout.prop(clpanel, "boundary_c")
 
+def get_cell(i1, i2):
+    """Helper function to easily get cell-property."""
+
+    def getter(self):
+        cell = get_active_cell()
+        if cell is not None:
+            return cell.array[i1, i2]
+        else:
+            return 0
+    return getter
+
+def set_cell(i1, i2):
+    """Helper function to easily set cell-property."""
+
+    def setter(self, value):
+        cell = get_active_cell()
+        if cell is not None:
+            cell[i1, i2] = value
+            
+    return setter
+
 
 class CellProperties(bpy.types.PropertyGroup):
 
@@ -61,18 +94,53 @@ class CellProperties(bpy.types.PropertyGroup):
     pbc: BoolProperty(
         name="pbc", default=True,
         description="pbc", update=Callback_modify_pbc)
-    cell_a: FloatVectorProperty(
-        name="a", default=(1, 0, 0),
-        # subtype = "XYZ",
-        description="Cell in a axis")
-    cell_b: FloatVectorProperty(
-        name="b", default=(0, 1, 0),
-        # subtype = "XYZ",
-        description="Cell in b axis")
-    cell_c: FloatVectorProperty(
-        name="c", default=(0, 0, 1),
-        # subtype = "XYZ",
-        description="Cell in c axis")
+    # getter/setter only used for float not float vector
+    cell_a0: FloatProperty(
+        name="a0", default=0,
+        description="Cell in a axis",
+        get=get_cell(0, 0),
+        set=set_cell(0, 0))
+    cell_a1: FloatProperty(
+        name="a1", default=0,
+        description="Cell in a axis",
+        get=get_cell(0, 1),
+        set=set_cell(0, 1))
+    cell_a2: FloatProperty(
+        name="a2", default=0,
+        description="Cell in a axis",
+        get=get_cell(0, 2),
+        set=set_cell(0, 2))
+    cell_b0: FloatProperty(
+        name="b0", default=0,
+        description="Cell in a axis",
+        get=get_cell(1, 0),
+        set=set_cell(1, 0))
+    cell_b1: FloatProperty(
+        name="b1", default=0,
+        description="Cell in a axis",
+        get=get_cell(1, 1),
+        set=set_cell(1, 1))
+    cell_b2: FloatProperty(
+        name="b2", default=0,
+        description="Cell in a axis",
+        get=get_cell(1, 2),
+        set=set_cell(1, 2))
+    cell_c0: FloatProperty(
+        name="c0", default=0,
+        description="Cell in a axis",
+        get=get_cell(2, 0),
+        set=set_cell(2, 0))
+    cell_c1: FloatProperty(
+        name="c1", default=0,
+        description="Cell in a axis",
+        get=get_cell(2, 1),
+        set=set_cell(2, 1))
+    cell_c2: FloatProperty(
+        name="c2", default=0,
+        description="Cell in a axis",
+        get=get_cell(2, 2),
+        set=set_cell(2, 2))
+    
     transform_a: IntVectorProperty(
         name="a", default=(1, 0, 0, 0), size=4,
         min=-20, max=20,
@@ -114,3 +182,10 @@ def modify_transform(name, transform):
     batoms = Batoms(label=name)
     batoms.transform(transform)
     batoms.hide = True
+
+def get_active_cell():
+    context = bpy.context
+    if context.object and context.object.batoms.type != 'OTHER':
+        cell = Bcell(label=context.object.batoms.label)
+        return cell
+    return None
