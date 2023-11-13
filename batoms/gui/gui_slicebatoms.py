ebaf84cc6c27848a7ed739355ff202d0b0ee2963
superstar54 <xingwang1991@gmail.com>
2023-11-13 11:08:11 +0100

    improve gui cell performance

--- batoms/gui/gui_slicebatoms.py
+++ batoms/gui/gui_slicebatoms.py
@@ -1,23 +1,22 @@
 import bpy
-from bpy.types import (Panel,
-                       )
-from bpy.props import (FloatProperty,
-                       FloatVectorProperty,
-                       BoolProperty,
-                       EnumProperty,
-                       )
+from bpy.types import (
+    Panel,
+)
+from bpy.props import (
+    FloatProperty,
+    BoolProperty,
+)
 from batoms.utils.butils import get_selected_vertices
-from batoms.slicebatoms import SliceBatoms
-from batoms.batoms import Batoms
+from batoms.utils.attribute import get_mesh_attribute_bmesh, set_mesh_attribute_bmesh
+
 
 class Batom_PT_prepare(Panel):
     bl_label = "Batom"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_options = {'DEFAULT_CLOSED'}
+    bl_options = {"DEFAULT_CLOSED"}
     bl_category = "Batoms"
     bl_idname = "BATOMS_PT_Batom"
-
 
     # @classmethod
     # def poll(cls, context):
@@ -36,73 +35,91 @@ class Batom_PT_prepare(Panel):
 
 
 # ---------------------------------------------------
-def get_active_batom():
+def get_selected_batom():
+    """Get the indices and the object of the selected vertices"""
+    # bpy.ops.object.mode_set(mode="OBJECT")
     context = bpy.context
-    if context.object and context.object.batoms.type != 'OTHER':
+    if context.object and context.object.batoms.type != "OTHER":
         indices = get_selected_vertices(context.object)
         if len(indices) > 0:
-            batoms = Batoms(label=context.object.batoms.label)
-            slicebatoms = batoms[indices]
-            return slicebatoms
+            return context.object, indices
         else:
-            return None
-    return None
+            return context.object, []
+    bpy.ops.object.mode_set(mode="EDIT")
+    return None, []
 
-def set_batom_attr(key, value):
-    """
-    """
-    batom = get_active_batom()
-    if batom is not None:
-        setattr(batom, key, value)
+
+def set_object_attr(key, value):
+    """set the attribute of the object"""
+    # bpy.ops.object.mode_set(mode="OBJECT")
+    obj, indices = get_selected_batom()
+    if obj is not None and len(indices) > 0:
+        for index in indices:
+            set_mesh_attribute_bmesh(obj, key, value, index=index)
         # bpy.context.view_layer.objects.active = batom.obj
+    bpy.ops.object.mode_set(mode="EDIT")
+
 
 def get_scale(self):
-    batom = get_active_batom()
-    if batom is not None:
-        return batom.scale[0]
+    obj, indices = get_selected_batom()
+    if obj is not None and len(indices) > 0:
+        return get_mesh_attribute_bmesh(obj, "scale", index=indices[0])[0]
     else:
         return 0
 
+
 def set_scale(self, value):
     self["scale"] = value
-    set_batom_attr("scale", value)
+    set_object_attr("scale", value)
 
 
+def get_show(self):
+    obj, indices = get_selected_batom()
+    if obj is not None and len(indices) > 0:
+        return bool(get_mesh_attribute_bmesh(obj, "show", index=indices[0])[0])
+    else:
+        return 0
+
+
+def set_show(self, value):
+    self["show"] = value
+    set_object_attr("show", value)
 
 
 class BatomProperties(bpy.types.PropertyGroup):
-
     def Callback_modify_size(self, context):
         batom = bpy.context.scene.batoms.batom
         size = batom.size
-        bpy.ops.batoms.batom_modify(key='size', size=size)
+        bpy.ops.batoms.batom_modify(key="size", size=size)
 
     def Callback_modify_bond(self, context):
         batom = bpy.context.scene.batoms.batom
         bond = batom.bond
-        bpy.ops.batoms.batom_modify(key='bond', bond=bond)
-
-    def Callback_modify_show(self, context):
-        batom = bpy.context.scene.batoms.batom
-        show = batom.show
-        bpy.ops.batoms.batom_modify(key='show', show=show)
+        bpy.ops.batoms.batom_modify(key="bond", bond=bond)
 
     scale: FloatProperty(
-        name="scale", default=0.6,
-        min=0, soft_max=2,
+        name="scale",
+        default=0.6,
+        min=0,
+        soft_max=2,
         description="scale",
         get=get_scale,
-        set=set_scale)
+        set=set_scale,
+    )
 
     size: FloatProperty(
-        name="size", default=1.5,
-        min=0, soft_max=4,
-        description="size", update=Callback_modify_size)
+        name="size",
+        default=1.5,
+        min=0,
+        soft_max=4,
+        description="size",
+        update=Callback_modify_size,
+    )
 
-    bond: BoolProperty(name="Bond", default=True,
-                       description="bond",
-                       update=Callback_modify_bond)
+    bond: BoolProperty(
+        name="Bond", default=True, description="bond", update=Callback_modify_bond
+    )
 
-    show: BoolProperty(name="Show", default=True,
-                       description="show",
-                       update=Callback_modify_show)
+    show: BoolProperty(
+        name="Show", default=True, description="show", get=get_show, set=set_show
+    )
