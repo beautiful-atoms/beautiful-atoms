353e06154c8017a49d1a4e5a62d818bbbc10ee30
T.Tian <alchem0x2a@gmail.com>
2024-07-30 00:40:31 +0800

    add help

--- build_extension.sh
+++ build_extension.sh
@@ -3,16 +3,22 @@
 # for Blender >= 4.2.
 # This should be run on all supported platforms including
 # linux-x86_64, windows-x86_64, macos-x86_64, macos_aarch64
-# 
-#
 # USAGE:
-# bash build_extension.sh [SOURCE_DIR]
-#
-# $SOURCE_DIR/build_requirements.txt will be used for external dependencies.
-# Other env variables to control:
-# BLENDER_BIN: path to blender binary, like /usr/local/bin/blender
-# EXPORT_DIR
-# Wheels from all platforms will be gathered to build the final extension.
+# bash build_extension.sh --source-dir SOURCE_DIR \
+#                         [--blender-bin BLENDER_BIN]\
+#                         [--build-dir BUILD_DIR]\
+#                         [--export-dir EXPORT_DIR]\
+#                         [--extra-wheels-directory EXTRA_WHEELS_DIR]
+
+# Options:
+# SOURCE_DIR: root directory of the batoms source code, e.g. ./batoms
+# BLENDER_BIN: path to blender executable on the current platform, default 
+# EXPORT_DIR: directory to export the generated zip files, default ./export
+# BUILD_DIR: directory for building the blender extension, default ./build (will be cleaned during building).
+# EXTRA_WHEELS_DIR: directory to wheels from other platforms.
+
+# You may need to set other variables like C_INCLUDE_PATH and CC for compiling wheels from source.
+
 
 set -e
 
@@ -45,6 +51,36 @@ check_blender_version() {
         exit 1
     fi
 }
+
+get_platform_string() {
+    local blender_py=$(get_blender_python_binary)
+    $blender_py <<EOF
+import platform
+import sys
+
+os_name = platform.system()
+arch = platform.machine()
+
+if os_name == "Linux":
+    if arch == "x86_64":
+        print("linux_x64")
+    else:
+        print("unsupported")
+elif os_name == "Darwin":
+    if arch == "x86_64":
+        print("macos_x64")
+    elif arch == "arm64":
+        print("macos_arm64")
+    else:
+        print("unsupported")
+elif os_name in ["Windows", "Microsoft"]:
+    print("windows_x64")
+else:
+    print("unsupported")
+EOF
+}
+
+
 
 # Get factory versions of python packages, like numpy
 get_blender_factory_packages() {
@@ -103,6 +139,39 @@ build_extension() {
 		--source-dir ${BUILD_DIR} \
 		--output-dir ${EXPORT_DIR} \
 		--split-platform
+
+    # If EXTRA_WHEELS_DIR not provided or empty,
+    # delete zips that are not for the current platform
+    if [ -z "${EXTRA_WHEELS_DIR}" ]; then
+        platform_string=$(get_platform_string)
+        if [ "$platform_suffix" == "unsupported" ]; then
+            echo "Unsupported platform or architecture."
+            exit 1
+        fi
+	echo "Extra wheels for other platforms not provided, will only build extension for current platform: $platform_string"
+        for zip_file in ${EXPORT_DIR}/*.zip; do
+            if [[ ! "$zip_file" == *"$platform_string".zip ]]; then
+                rm -f "$zip_file"
+            fi
+        done
+    else
+	local num_wheels=-1
+	# TODO: better to fetch label batoms from toml file
+        for zip_file in ${EXPORT_DIR}/batoms-*.zip; do
+            current_num_wheels=$(unzip -l "$zip_file" | awk '/wheels\/.*\.whl/ {print $NF}' | wc -l)
+            if [ $num_wheels -eq -1 ]; then
+                num_wheels=$current_num_wheels
+            else
+                if [ $current_num_wheels -ne $num_wheels ]; then
+                    echo "Mismatch found in the number of wheels in extensions between platforms."
+		    echo "Please check if $EXTRA_WHEEKS_DIR contains the correct wheel files."
+		    echo "Will now abort."
+                    exit 1
+                fi
+            fi
+        done
+    fi
+    echo "Building complete. The following extensions can be found at ${EXPORT_DIR}"
 }
 
 usage() {
@@ -177,8 +246,6 @@ main() {
 
     update_manifest "$WHEELS_DIR"
     build_extension
-    
-    
 }
 
 main "$@"
